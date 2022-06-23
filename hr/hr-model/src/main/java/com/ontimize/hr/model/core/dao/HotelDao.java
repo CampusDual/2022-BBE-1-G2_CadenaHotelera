e1baa0360ad116cb794c294a386fd8214f40707d
marioplaza <mario1plaza@gmail.com>
2022-06-23 13:56:35 +0200

    Esqueleto con Entidad Hotel

---
+++ hr/hr-model/src/main/java/com/ontimize/hr/model/core/dao/HotelDao.java
@@ -0,0 +1,23 @@
+package com.ontimize.hr.model.core.dao;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+
+@Repository("HotelDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/HotelDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+public class HotelDao extends OntimizeJdbcDaoSupport {
+
+	public static final String ATTR_ID = "htl_id";
+	public static final String ATTR_NAME = "htl_name";
+	public static final String ATTR_CITY = "htl_city";
+	public static final String ATTR_ADDRESS = "htl_address";
+	public static final String ATTR_ZIPCODE = "htl_zip_code";
+	public static final String ATTR_EMAIL = "htl_email";
+	public static final String ATTR_PHONE = "htl_phone";
+	public static final String ATTR_STARS = "htl_stars";
+
+}
