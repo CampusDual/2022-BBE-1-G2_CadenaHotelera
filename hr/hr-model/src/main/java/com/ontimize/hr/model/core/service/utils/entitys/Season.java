7cbd6d384fbe88bef84721c0b68056f950d53d37
marioplaza <mario1plaza@gmai.com>
2022-07-16 16:48:01 +0200

    entity season

---
+++ hr/hr-model/src/main/java/com/ontimize/hr/model/core/service/utils/entitys/Season.java
@@ -0,0 +1,45 @@
+package com.ontimize.hr.model.core.service.utils.entitys;
+
+import java.util.Date;
+
+public class Season {
+	
+	private Date startDate;
+	
+	private Date endDate;
+	
+	private Integer multiplier;
+
+	public Season(Date startDate, Date endDate, Integer multiplier) {
+		super();
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.multiplier = multiplier;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Integer getMultiplier() {
+		return multiplier;
+	}
+
+	public void setMultiplier(Integer multiplier) {
+		this.multiplier = multiplier;
+	}
+	
+	
+}
