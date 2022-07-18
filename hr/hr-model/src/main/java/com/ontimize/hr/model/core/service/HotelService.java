95f8bbaf2b60455ecf10f5d4a353b89edd4ec5d4
Miguel López Patricio <mlpatri@campusdual.com>
2022-07-18 19:50:38 +0200

    Tests de HotelService

--- hr/hr-model/src/main/java/com/ontimize/hr/model/core/service/HotelService.java
+++ hr/hr-model/src/main/java/com/ontimize/hr/model/core/service/HotelService.java
@@ -6,12 +6,15 @@ import java.util.Map.Entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
 import com.ontimize.hr.api.core.service.IHotelService;
 import com.ontimize.hr.model.core.dao.HotelDao;
+import com.ontimize.hr.model.core.service.utils.CredentialUtils;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
@@ -21,9 +24,20 @@ import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 @Service("HotelService")
 @Lazy
 public class HotelService implements IHotelService {
+	
+	public static final String WRONG_DATA_INPUT = "WRONG DATA INPUT";
+
+	public static final String WRONG_STARS = "ONLY STARS BETWEEN 1 AND 5 ALLOWED";
+
+	public static final String HOTEL_NAME_BLANK = "HOTEL NAME CAN'T BE BLANK";
+
+	public static final String DUPLICATED_HOTEL_MAIL = "DUPLICATED HOTEL MAIL";
 
 	@Autowired
 	private HotelDao hotelDao;
+	
+	@Autowired
+	private CredentialUtils credential;
 
 	@Autowired
 	private DefaultOntimizeDaoHelper daoHelper;
@@ -32,8 +46,29 @@ public class HotelService implements IHotelService {
 	@Secured({ PermissionsProviderSecured.SECURED })
 	public EntityResult hotelQuery(Map<String, Object> keyMap, List<String> attrList)
 			throws OntimizeJEERuntimeException {
-		Map<Object,Object> mapita = daoHelper.getUser().getOtherData();
-		return this.daoHelper.query(this.hotelDao, keyMap, attrList);
+		
+		String user = daoHelper.getUser().getUsername();
+		
+		if(credential.isUserEmployee(user)) {
+			
+			Integer hotelId = credential.getHotelFromUser(user);
+			
+			if(hotelId !=-1) {
+				
+				keyMap.remove(HotelDao.ATTR_ID);
+				keyMap.put(HotelDao.ATTR_ID,hotelId);
+				
+			}
+		}
+		
+		try {
+			
+			return this.daoHelper.query(this.hotelDao, keyMap, attrList);
+		} catch(BadSqlGrammarException e) {
+			
+			return new EntityResultMapImpl(EntityResult.OPERATION_WRONG, 12, WRONG_DATA_INPUT);
+		}
+		
 
 	}
 
@@ -45,16 +80,30 @@ public class HotelService implements IHotelService {
 
 		if(attrMap.containsKey(HotelDao.ATTR_STARS)) {
 			Integer stars =  (Integer)attrMap.get(HotelDao.ATTR_STARS);
-			if(stars<1 ||stars>5) return new EntityResultMapImpl(EntityResult.OPERATION_WRONG, 12,"ONLY STARS BETWEEN 1 AND 5 ALLOWED");
+			if(stars<1 ||stars>5) return new EntityResultMapImpl(EntityResult.OPERATION_WRONG, 12,WRONG_STARS);
 		}		
 
 		try {
 			return this.daoHelper.insert(this.hotelDao, attrMap);
 		} catch (DuplicateKeyException e) {
 			res.setCode(EntityResult.OPERATION_WRONG);
-			res.setMessage("DUPLICATED HOTEL NAME");
+			res.setMessage(DUPLICATED_HOTEL_MAIL);
 			return res;
+		} catch (DataIntegrityViolationException e) {
+			
+			if(e.getMessage() != null &&  e.getMessage().contains("ck_hotel_htl_name")) {
+				res.setCode(EntityResult.OPERATION_WRONG);
+				res.setMessage(HOTEL_NAME_BLANK);
+				return res;				
+			}
+			
+			res.setCode(EntityResult.OPERATION_WRONG);
+			res.setMessage("DATA INTEGRITY VIOLATION");
+			return res;		
 		}
+	
+		
+		
 	}
 
 	@Override
@@ -65,10 +114,10 @@ public class HotelService implements IHotelService {
 		EntityResult res = new EntityResultMapImpl();
 
 		for (Entry<String, Object> entry : attrMap.entrySet()) {
-				if (entry.getKey().equals(HotelDao.ATTR_STARS) &&(Integer.parseInt(entry.getValue().toString()) < 1)
-						|| (Integer.parseInt(entry.getValue().toString()) > 5)) {
+				if (entry.getKey().equals(HotelDao.ATTR_STARS) &&((Integer.parseInt(entry.getValue().toString()) < 1)
+						|| (Integer.parseInt(entry.getValue().toString()) > 5))) {
 					res.setCode(EntityResult.OPERATION_WRONG);
-					res.setMessage("ONLY STARS BETWEEN 1 AND 5 ALLOWED");
+					res.setMessage(WRONG_STARS);
 					return res;
 				}
 		}
@@ -79,7 +128,20 @@ public class HotelService implements IHotelService {
 			res.setCode(EntityResult.OPERATION_WRONG);
 			res.setMessage("DUPLICATED HOTEL NAME");
 			return res;
+		}catch (DataIntegrityViolationException e) {
+			
+			if(e.getMessage() != null &&  e.getMessage().contains("ck_hotel_htl_name")) {
+				res.setCode(EntityResult.OPERATION_WRONG);
+				res.setMessage(HOTEL_NAME_BLANK);
+				return res;				
+			}
+			
+			res.setCode(EntityResult.OPERATION_WRONG);
+			res.setMessage("DATA INTEGRITY VIOLATION");
+			return res;		
 		}
+		
+		
 
 	}
 
--- hr/hr-model/src/test/java/com/ontimize/hr/model/core/service/HotelTest.java
+++ hr/hr-model/src/test/java/com/ontimize/hr/model/core/service/HotelTest.java
@@ -3,38 +3,188 @@ package com.ontimize.hr.model.core.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.util.Assert;
 
 import com.ontimize.hr.model.core.dao.HotelDao;
+import com.ontimize.hr.model.core.dao.RoomTypeDao;
+import com.ontimize.hr.model.core.service.utils.CredentialUtils;
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.dto.EntityResultMapImpl;
+import com.ontimize.jee.common.services.user.UserInformation;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 
 
-
+@ExtendWith(MockitoExtension.class)
 class HotelTest {
 
 	@Mock
-	private HotelDao hotelDao;
-	
-	@Mock
 	private DefaultOntimizeDaoHelper daoHelper;
+	
+	@MockBean
+	private HotelDao hotelDao;
 	
 	@InjectMocks
 	private HotelService service;
 	
-	@BeforeAll
-	void setup() {
+	@Mock
+	private CredentialUtils credential;
+	
+	
+	// Test en pruebas 
+	/*
+	@Test
+	@DisplayName("Query Test")
+	void queryTest() {
+		
+		Map<String, Object> filter = new HashMap<>(); // filtro 
+		
+		filter.put(hotelDao.ATTR_NAME, "Hotel Chungo");
+		
+		List<String> columns = Arrays.asList(hotelDao.ATTR_ID); // resultado de la consulta
+	
+		UserInformation userInfo = new UserInformation();
+
+		when(daoHelper.getUser()).thenReturn(userInfo);
+		
+		// when(userInfo.getUsername()).thenReturn("Mister X");
+				
+		when(credential.isUserEmployee("")).thenReturn(false); // el usuario es el correcto
+				
+		when(daoHelper.query(hotelDao, filter, columns)).thenThrow(new BadSqlGrammarException(null, null, null));
+		
+		assertEquals(HotelService.WRONG_DATA_INPUT, service.hotelQuery(filter, columns).getMessage());
+		
+	}
+*/
+	
+	
+	
+	@Test
+	@DisplayName("Inserting wrong number of stars")
+	void insertWrongNumberOfStarsTest() {
+		
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put(HotelDao.ATTR_NAME, "Hotel Chungo");
+		data.put(HotelDao.ATTR_ADDRESS, "C/PRUEBA, 3");
+		data.put(HotelDao.ATTR_CITY, "Las Vegas");
+		data.put(HotelDao.ATTR_EMAIL, "hotel@correo.com");
+		data.put(HotelDao.ATTR_PHONE, "23445567");
+		data.put(HotelDao.ATTR_STARS, 45);
+		
+		EntityResult res = new EntityResultMapImpl(EntityResult.OPERATION_WRONG, 12, HotelService.WRONG_STARS);
+		
+		// when(daoHelper.insert(hotelDao, data)).thenReturn(res);
+		assertEquals(HotelService.WRONG_STARS,service.hotelInsert(data).getMessage());
+	}
+	
+	
+	
+	@Test
+	@DisplayName("Inserting blank hotel type name")
+	void InsertEmptyHotelNameTest() {
+		
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put(HotelDao.ATTR_NAME, "");
+		data.put(HotelDao.ATTR_ADDRESS, "C/PRUEBA, 3");
+		data.put(HotelDao.ATTR_CITY, "Las Vegas");
+		data.put(HotelDao.ATTR_EMAIL, "hotel@correo.com");
+		data.put(HotelDao.ATTR_PHONE, "23445567");
+		
+		
+		when(daoHelper.insert(hotelDao, data)).thenThrow(new DataIntegrityViolationException("ck_hotel_htl_name"));
+		assertEquals(HotelService.HOTEL_NAME_BLANK,service.hotelInsert(data).getMessage());
+	}
+	
+	
+	
+	
+	
+	@Test
+	@DisplayName("Inserting duplicate hotel mail")
+	void InsertDuplicateMailTest() {
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put(HotelDao.ATTR_NAME, "Hotel Las Vegas");
+		data.put(HotelDao.ATTR_ADDRESS, "C/PRUEBA, 3");
+		data.put(HotelDao.ATTR_CITY, "Las Vegas");
+		data.put(HotelDao.ATTR_EMAIL, "hotelvegas@mail.com");
+		data.put(HotelDao.ATTR_PHONE, "23445567");
+
+		
+		when(daoHelper.insert(hotelDao, data)).thenThrow(new DuplicateKeyException("uq_hotel_htl_email"));
+		assertEquals(HotelService.DUPLICATED_HOTEL_MAIL,service.hotelInsert(data).getMessage());
+		
+	}
+	
+	
+	@Test
+	@DisplayName("Updating with wrong number of stars")
+	void UpdateWrongStarsTest() {
+		
+		
+		// DATOS NUEVOS
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put(HotelDao.ATTR_STARS, 45);
+		
+		// LO QUE SE BUSCA -> CONDICION
+		Map<String, Object> filter = new HashMap<String, Object>();
+		filter.put(HotelDao.ATTR_NAME, "Hotel Chungo");
+		
+		
+		
+		EntityResult res = new EntityResultMapImpl(EntityResult.OPERATION_WRONG, 12, HotelService.WRONG_STARS);
+		
+		// when(daoHelper.insert(hotelDao, data)).thenReturn(res);
+		assertEquals(HotelService.WRONG_STARS,service.hotelUpdate(data,filter).getMessage());
+		
+		
 		
 	}
 	
 	@Test
-	void CreateHotelTest() {
+	@DisplayName("Updating with blank name")
+	void UpdateBlankNameTest() {
 		
 		
-		when(daoHelper.query(hotelDao, null, null)).thenReturn(null);
-//		service.hotelDelete();
+		// DATOS NUEVOS
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put(HotelDao.ATTR_NAME,"");
+		
+		// LO QUE SE BUSCA -> CONDICION
+		Map<String, Object> filter = new HashMap<String, Object>();
+		filter.put(HotelDao.ATTR_NAME, "Hotel Chungo");
+		
+		
+		
+		// EntityResult res = new EntityResultMapImpl(EntityResult.OPERATION_WRONG, 12, HotelService.WRONG_STARS);
+		
+		when(daoHelper.update(hotelDao, data, filter)).thenThrow(new DataIntegrityViolationException("ck_hotel_htl_name"));
+		assertEquals(HotelService.HOTEL_NAME_BLANK,service.hotelUpdate(data,filter).getMessage());
+			
+	}
+	
+	@Test
+	void DeleteTest() {
 		
 	}
 }
